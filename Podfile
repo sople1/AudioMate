95b429899dc59aec860b9357f7effe2caaa70fc8
Ruben Nine <sonicbee9@gmx.com>
2014-07-27 04:26:01 +0200

    Removing yoursway-create-dmg, LetsMove, and StartAtLoginController from source. Re-adding them either as git submodules or Cocoapod dependencies.

--- Podfile
+++ Podfile
@@ -1,8 +1,10 @@
 platform :osx, "10.7"
 
 target "AudioMate" do
-  pod 'LVDebounce', '~> 0.0.4'
   pod 'AMCoreAudio', '~> 1.3.2'
+  pod 'LetsMove', '~> 1.9'
+  pod 'StartAtLoginController', '~> 0.0.1'
+  pod 'LVDebounce', '~> 0.0.4'
 end
 
 target "AudioMateLauncher" do
--- Scripts/generate-dmg.rb
+++ Scripts/generate-dmg.rb
@@ -6,5 +6,5 @@ appName = ENV['PRODUCT_NAME']
 exec %(
        cd "#{appPath}"
        test -f *.dmg && rm *.dmg
-       bash #{ENV['SRCROOT']}/yoursway-create-dmg/create-dmg --window-size 603 512 --window-pos 150 250 --background #{ENV['SRCROOT']}/yoursway-create-dmg/bg.tiff --icon-size 128 --volname "#{appName} v#{CFBundleShortVersionString}" --icon "#{appName}" 130 280 --app-drop-link 432 160 #{appName}-v#{CFBundleShortVersionString}.dmg .
+       bash #{ENV['SRCROOT']}/ExternalDeps/yoursway-create-dmg/create-dmg --window-size 603 512 --window-pos 150 250 --background #{ENV['SRCROOT']}/Artwork/dmg-bg.tiff --icon-size 128 --volname "#{appName} v#{CFBundleShortVersionString}" --icon "#{appName}" 130 280 --app-drop-link 432 160 #{appName}-v#{CFBundleShortVersionString}.dmg .
        )
